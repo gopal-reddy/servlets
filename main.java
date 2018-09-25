cd30f8fc326a64342a7900cc278f97f59628e62b
gopal reddy <gopalreddy0078@gmail.com>
2018-09-25 23:15:02 +0530

    working

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.*;
+class main
+{
+public static void main(String args[])
+{
+try
+{
+Class.forName("com.mysql.jdbc.Driver");
+Connection conn = null;
+conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testtable","root", "");
+System.out.print("Database is connected !");
+conn.close();
+}
+catch(Exception e)
+{
+System.out.print("Do not connect to DB - Error:"+e);
+}
+}
+}
